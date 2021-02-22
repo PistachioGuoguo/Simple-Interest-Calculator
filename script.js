25dd3a15f6633cfa42e26293a2adff5417828091
Pistachio Guoguo <sangzizhou@gmail.com>
2021-02-22 13:55:01 -0600

    Final edits

--- script.js
+++ script.js
@@ -3,21 +3,40 @@ const summary = document.querySelector('#result');
 
 function compute()
 {
-    principal = parseFloat(document.getElementById("principal").value) ;
+    principal = parseFloat(document.getElementById("principal").value);
     interestRate = parseFloat(document.getElementById("interest").value) / 100;
     years = parseInt(document.getElementById("years").value);
 
-    currentYear = new Date().getFullYear();
+    if (isNaN(principal) || principal <= 0){
+        alert("Princicpal must be a positive number");
+        document.getElementById("principal").focus();
+        summary.innerHTML = ``;
+    }else{
 
-    // total = principal + principal * interestRate * years;  
-    total = principal * interestRate * years;
+        if (isNaN(years)){
+            alert("Please select the investment year");
+            document.getElementById("years").focus();
+            summary.innerHTML = ``;
+        }else{
+            currentYear = new Date().getFullYear();
+        
+            // total = principal + principal * interestRate * years;  
+            total = principal * interestRate * years;
+        
+            summary.innerHTML = `
+            If you deposit ${principal},<br>
+            at an interest rate of ${highlight(interestRate * 100 + '%')}.<br>
+            You will receive an amount of ${highlight(total)} ,<br>
+            in the year ${highlight(currentYear + years)}.<br>
+            `;
+        }
 
-    summary.innerHTML = `
-    If you deposit ${principal},<br>
-    at an interest rate of ${interestRate * 100}%.<br>
-    You will receive an amount of ${total} ,<br>
-    in the year ${currentYear + years}.<br>
-    `;
+    }
+
+}
+
+function highlight(str){
+    return `<span style="background-color: #FFFF00">${str}</span>`;
 
 }
         
